0beb540f96368b8f947596b829973e7ab3cec6d4
Renata Karolina <renatakarolinarko@gmail.com>
2022-12-21 10:40:51 -0400

    making texts responsive

--- src/components/mains.tsx
+++ src/components/mains.tsx
@@ -1,13 +1,11 @@
 import styled from "styled-components";
-import Link from "next/link";
-import Image from "next/image";
 
 const Container = styled.div`
   width: 100%;
-  padding: 2rem 2rem;
+  padding: 0 2rem;
   display: flex;
-  align-items: center;
-  // margin-top: 5rem;
+  flex-direction: column;
+  margin-top: 4rem;
 
   @media (max-width: 655px) {
     a {
@@ -16,40 +14,50 @@ const Container = styled.div`
   }
 
   @media (max-width: 470px) {
-    p {
-      font-size: 1.2rem;
-    }
-  }
-  @media (max-width: 432px) {
-    padding: 0rem 1rem;
-
-    p {
-      font-size: 1rem;
-    }
+    padding: 0 1rem;
   }
 `;
 
 const ContainerText = styled.div`
+  width: 40rem;
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
 
-  @media (max-width: 470px) {
+  @media (max-width: 625px) {
     width: 100%;
-    align-items: center;
+  }
+
+  @media (max-width: 510px) {
+    width: 20rem;
+  }
+
+  @media (max-width: 340px) {
+    width: 100%;
   }
 `;
 
 const Title = styled.h1`
   color: ${({ theme }) => theme.colors.secondary};
   font-size: 2.6rem;
+  margin: 0;
 
   ::selection {
     color: ${({ theme }) => theme.colors.purpleLigth};
     background: none;
   }
+
+  @media (max-width: 625px) {
+    font-size: 2rem;
+  }
+
+  @media (max-width: 340px) {
+    font-size: 1.5rem;
+  }
+
 `;
 
-const SubTitle = styled.h1`
+const SubTitle = styled.h2`
   font-size: 2.5rem;
   color: ${({ theme }) => theme.colors.purpleLigth};
   margin: 0;
@@ -58,10 +66,18 @@ const SubTitle = styled.h1`
     color: ${({ theme }) => theme.colors.purple};
     background: none;
   }
+
+  @media (max-width: 625px) {
+    font-size: 2rem;
+  }
+
+  @media (max-width: 340px) {
+    font-size: 1.5rem;
+  }
 `;
 
 const Description = styled.p`
-  width: 100%;
+  width: 40rem;
   color: ${({ theme }) => theme.colors.secondary};
 
   ::selection {
@@ -69,42 +85,11 @@ const Description = styled.p`
     background: none;
   }
 
-  @media (max-width: 320px) {
-    line-height: 1.8rem;
+  @media (max-width: 625px) {
+    width: 100%;
   }
+
 `;
-
-const ContainerTextAndA = styled.div`
-  width: 90%;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  gap: 30px;
-`;
-
-const LinkExtern = styled.a`
-  display: none;
-  background: rgb(221, 151, 219);
-  width: 20rem;
-  padding: 8px;
-  border-radius: 15px;
-  text-align: center;
-  transition: all 0.2s;
-  cursor: pointer;
-
-  &:hover {
-    box-shadow: 0px 5px 8px 0px rgba(40, 36, 40, 0.2) !important;
-    transform: scale(1.1);
-  }
-`;
-
-const StyledA = styled.a``;
-
-const StyledLink = ({ href, name }) => (
-  <Link href={href} passHref>
-    <StyledA>{name}</StyledA>
-  </Link>
-);
 
 export default function Mains() {
   return (
@@ -112,9 +97,9 @@ export default function Mains() {
         <ContainerText>
           <Title>
             Olá! Sou Renata,
-            <SubTitle>Desenvolvedora Front-end.</SubTitle>
           </Title>
-
+          <SubTitle>Desenvolvedora Front-end.</SubTitle>
+        </ContainerText>
           <Description>
             Iniciei a transição para a área de desenvolvimento de software em
             Abril de 2022 estudando lógica de programação e algoritmos,
@@ -122,14 +107,7 @@ export default function Mains() {
             <br />
             <br /> Atualmente, desenvolvo projetos com a biblioteca React com
             seu ecossistema e o framework Next.js.
-          </Description>
-
-          <ContainerTextAndA>
-            <LinkExtern>
-              <StyledLink href="/projects" name="Projetos" />
-            </LinkExtern>
-          </ContainerTextAndA>
-        </ContainerText>
+          </Description>        
       </Container>
   );
 }
