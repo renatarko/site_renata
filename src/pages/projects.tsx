cafc52e38fde0f6abbc0e7391e1091379821e0ab
Renata Karolina <renatakarolinarko@gmail.com>
2022-12-30 11:17:54 -0400

    add icons and descriptions on cards, removed containerCard

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,50 +1,56 @@
 import Link from "next/link";
 import styled from "styled-components";
 import { FaArrowLeft } from "react-icons/fa";
-import { TbWorld } from "react-icons/tb";
-import { IconBase } from "react-icons";
 import MenuMobile from "../components/menuMobile";
+import { FaGithubAlt, FaCalculator, FaCheckCircle, FaMugHot, FaStoreAlt, FaIdBadge } from "react-icons/fa";
 
 const myProjects = [
   {
     id: 1,
     name: "Minhas Tarefas",
-    image: "/projects/1.png",
+    description: "Alguma descrição sobre o site aqui",
     url: "https://minhastarefasapp.netlify.app/",
+    icon: <FaCheckCircle/>
   },
   {
     id: 2,
     name: "GitHub Repositories",
-    image: "/projects/2.png",
+    description: "Alguma descrição sobre o site aqui",
     url: "https://github-tracker-rko.netlify.app/",
+    icon: <FaGithubAlt/>
   },
   {
     id: 3,
-    name: "Formulário de Cadastro",
-    image: "/projects/3.png",
+    name: <p>Formulário <br/> de Cadastro</p>,
+    description: "Alguma descrição sobre o site aqui",
     url: "https://renatarko.github.io/formulario/",
+    icon: <FaIdBadge/>,
   },
   {
     id: 4,
-    name: "Site Empresa",
-    image: "/projects/4.png",
-    url: "https://github.com/renatarko/beatysalon/blob/main/readme.gif",
+    name: "Site Salão de Beleza",
+    description: "Alguma descrição sobre o site aqui",
+    url: "https://renatarko.github.io/beatysalon/",
+    icon: <FaStoreAlt/>
   },
   {
     id: 5,
     name: "Calculadora IMC",
-    image: "/projects/5.png",
+    description: "Alguma descrição sobre o site aqui",
     url: "https://calculadoraimcapp.netlify.app/",
+    icon: <FaCalculator/>
   },
   {
     id: 6,
-    name: "Site Empresa",
-    image: "/projects/6.png",
-    url: "https://biancoea.netlify.app/",
+    name: "Site Cafeteria",
+    description: "Alguma descrição sobre o site aqui",
+    url: "https://cafeteriaapp.vercel.app/",
+    icon: <FaMugHot/>
   },
 ];
 
-export const Main = styled.main`
+const Main = styled.main`
+  height: 100vh;
   display: flex;
   flex-direction: column;
   margin: 0px auto;
@@ -60,14 +66,12 @@ const Header = styled.header`
   align-items: center;
   padding: 0 2rem;
   background: none;
-  z-index: 1;
   `;
 
 const Title = styled.h1`
   color: ${({ theme }) => theme.colors.purple};
   text-transform: uppercase;
   font-size: 1.3rem;
-  z-index: 1;
 
   @media (max-width: 430px) {
     font-size: 1rem;
@@ -75,8 +79,11 @@ const Title = styled.h1`
 `;
 
 const ContainerGrid = styled.div`
+  background: transparent;
+  width: 100%;
   display: grid;
   margin-top: 5rem;
+  margin-bottom: 2rem;
   grid-template-columns: 1fr 1fr 1fr;
   justify-items: center;
   align-items: center;
@@ -91,60 +98,75 @@ const ContainerGrid = styled.div`
   }
 `;
 
-const ContainerCard = styled.a`
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Card = styled.div`
+const Card = styled.a`
   width: 11rem;
   height: 10rem;
+  
   background: #1d1d1d;
   border-radius: 10px;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  gap: 1rem;
 
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  z-index: 0;
+  text-align: center;
+  position: relative;
 
   cursor: pointer;
-  transition: all 0.3s;
+  transition: all 0.5s;
+
+  > a {
+    color: ${({ theme }) => theme.colors.secondary};
+    transition: all 0.3s;
+    font-size: .9rem;
+    position: absolute;
+    left:0;
+    right: 0;
+    bottom: 20px;
+    font-weight: 600;
+    opacity: 1;
+    transition: .5s;
+  }
+
+  > span {
+    color: ${({theme}) => theme.colors.secondary};
+    font-size: 10px;
+    position: absolute;
+    left:0;
+    bottom: 0;
+    opacity: 0;
+    transition: .5s;
+  }
+
+  > svg {
+    font-size: 2rem;
+    color: ${({theme}) => theme.colors.purple};
+    position: absolute;
+    top: 3rem;
+    left: 4.5rem;
+    right: 0;
+    bottom: 0;
+    transition: .5s;
+  }
 
   &:hover {
     transform: scale(1.03);
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    background: #1d1d1d;
+    box-shadow: rgba(0, 0, 0, 0.432) 0px 5px 15px;
+    border: 1px solid ${({theme}) => theme.colors.purple};
   }
 
-    &:hover > p {
-      padding: .3rem;
-      background: ${({theme}) => theme.colors.secondary};
-      color: ${({theme}) => theme.colors.purple};
-      font-weight: 600;
+    &:hover a {
+      opacity: 1;
+      bottom: 6rem;
     }
 
-    &:hover > svg {
-      display: block
+    &:hover span {
+      bottom: 3rem;
+      opacity: 1;
     }
-`;
 
-const IconWorld = styled(TbWorld)`
-  font-size: 1.8rem;
-  color: ${({ theme }) => theme.colors.purpleLigth};
-  display: none;
-`;
-
-const Paragraph = styled.p`
-  color: ${({ theme }) => theme.colors.purpleLigth};
-  border-radius: 10px;
-  transition: all 0.3s;
-  font-size: .8rem;
+    &:hover svg {
+      font-size: 1rem;
+      top: 10px;
+      left: 10px;
+    }
 `;
 
 const IconBack = styled(FaArrowLeft)`
@@ -158,43 +180,10 @@ const IconBack = styled(FaArrowLeft)`
   }
 `
 
-const BackOne = styled.div`
-  animation: slide 5s ease-in-out infinite alternate;
-  background-image: linear-gradient(-60deg, #1a1a1ae8 50%, rgba(82, 48, 116, 0.301) 50%);
-  bottom:0;
-  left:-50%;
-  opacity:.1;
-  position:fixed;
-  right:-50%;
-  top:0;
-  z-index:0;
-
-  @keyframes slide {
-  0% {
-    transform:translateX(-25%);
-  }
-  100% {
-    transform:translateX(25%);
-  }
-}
-`
-
-const BacktTwo = styled(BackOne)`
-  animation-direction: alternate-reverse;
-  animation-duration:4s;
-`
-
-const BackThree = styled(BackOne)`
-  animation-duration:5s;
-`
-
 export default function Projects() {
   return (
     <>
       <Main>
-      <BackOne/>
-      <BacktTwo/>
-      <BackThree/>
       <Header>
         <Link href="/">
           <IconBack/>
@@ -204,15 +193,13 @@ export default function Projects() {
       </Header>
       <ContainerGrid>
         {myProjects.map((project) => (
-          <ContainerCard key={project.id} href={project.url} target="_blank">
-            <Card>
-              <IconWorld/>
-              <Paragraph>{project.name}</Paragraph>
+            <Card key={project.id} href={project.url}>
+              {project.icon}
+              <a href={project.url} target="_blank">{project.name}</a>
+              <span>{project.description}</span>
             </Card>
-          </ContainerCard>
         ))}
       </ContainerGrid>
-      
     </Main>
     </>
   );
