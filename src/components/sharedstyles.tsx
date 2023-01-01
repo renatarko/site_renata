af0802394f9a1f681366fc972222018b5dab22f3
Renata Karolina <renatakarolinarko@gmail.com>
2023-01-01 02:14:26 -0400

    fix styles

--- src/components/sharedstyles.tsx
+++ src/components/sharedstyles.tsx
@@ -5,18 +5,18 @@ const Main = styled.main`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 0px auto;
+  margin: 0 auto;
   width: 100%;
   height: auto;
   max-width: 1200px;
 `;
-const Container = styled.div`
-  height: auto;
+const Container = styled.section`
   width: 100%;
+  height: 100vh;
 
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  /* justify-content: center; */
   align-items: center;
 `;
 
