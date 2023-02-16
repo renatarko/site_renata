09795ca67c04fee04702fcc2d32581043c9ffb68
Renata Karolina <renatakarolinarko@gmail.com>
2023-02-16 11:21:35 -0400

    added a loanding package and added it to projects in progress

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,58 +1,87 @@
 import Link from "next/link";
 import styled from "styled-components";
-import { FaArrowLeft } from "react-icons/fa";
+import { FaArrowLeft, FaCode } from "react-icons/fa";
 import MenuMobile from "../components/menuMobile";
-import { FaGithubAlt, FaCalculator, FaCheckCircle, FaMugHot, FaStoreAlt, FaIdBadge, FaBookmark } from "react-icons/fa";
+import {
+  FaGithubAlt,
+  FaCalculator,
+  FaCheckCircle,
+  FaMugHot,
+  FaStoreAlt,
+  FaIdBadge,
+  FaBookmark,
+} from "react-icons/fa";
+import ReactLoading from "react-loading";
 
+let id = Math.floor(Date.now() * Math.random()).toString(36);
 const myProjects = [
   {
-    id: 1,
+    id: id,
     name: "Minhas Tarefas",
     description: `Projeto "to do" onde tive o primeiro contato com hooks do React. Feito com React e ViteJS`,
     url: "https://minhastarefasapp.netlify.app/",
-    icon: <FaCheckCircle/>
+    icon: <FaCheckCircle />,
   },
   {
-    id: 2,
+    id: id,
     name: "GitHub Repositories",
-    description: "Primeiro projeto com React. A aplicação lista os repositórios dos usuários do GitHub",
+    description:
+      "Primeiro projeto com React. A aplicação lista os repositórios dos usuários do GitHub",
     url: "https://github-tracker-rko.netlify.app/",
-    icon: <FaGithubAlt/>
+    icon: <FaGithubAlt />,
   },
   {
-    id: 3,
-    name: <p>Formulário <br/> de Cadastro</p>,
+    id: id,
+    name: <p style={{ fontSize: "1.15rem" }}>Formulário de Cadastro</p>,
     description: "Formulário com validações utilizando HTML, CSS e JS",
     url: "https://renatarko.github.io/formulario/",
-    icon: <FaIdBadge/>,
+    icon: <FaIdBadge />,
   },
   {
-    id: 4,
+    id: id,
     name: "Site Salão de Beleza",
-    description: "Projeto desenvolvido durante a NLW Origin da Rocketseat, utilizando HTML, CSS e JS",
+    description:
+      "Projeto desenvolvido durante a NLW Origin da Rocketseat, utilizando HTML, CSS e JS",
     url: "https://renatarko.github.io/beatysalon/",
-    icon: <FaStoreAlt/>
+    icon: <FaStoreAlt />,
   },
   {
-    id: 5,
+    id: id,
     name: "Calculadora IMC",
-    description: "Primeiro projeto autônomo assim que iniciei os estudos na área. Feito com HTML, CSS e JS",
+    description:
+      "Primeiro projeto autônomo assim que iniciei os estudos na área. Feito com HTML, CSS e JS",
     url: "https://calculadoraimcapp.netlify.app/",
-    icon: <FaCalculator/>
+    icon: <FaCalculator />,
   },
   {
-    id: 6,
+    id: id,
     name: "Site Cafeteria",
-    description: "Landing Page sobre uma cafeteria fictícia que criei do zero, feito com HTML, CSS e JS",
+    description:
+      "Landing Page sobre uma cafeteria fictícia que criei do zero, feito com HTML, CSS e JS",
     url: "https://cafeteriaapp.vercel.app/",
-    icon: <FaMugHot/>
+    icon: <FaMugHot />,
   },
   {
-    id:7,
-    name:"Site Book",
+    id: id,
+    name: "Site Book",
     description: "Página de Compra de Livro feito com HTML, CSS e JS",
     url: "https://redexbook.netlify.app",
-    icon: <FaBookmark/>
+    icon: <FaBookmark />,
+  },
+  {
+    id: id,
+    name: "Em Progresso",
+    description: <ReactLoading color={"rgb(130, 92, 168)"} width={50} />,
+    url: "",
+    icon: <FaCode />,
+  },
+  {
+    id: id,
+    name: "Em Progresso",
+    description: <ReactLoading color={"rgb(130, 92, 168)"} width={50} />,
+    url: "",
+    icon: <FaCode />,
+  },
 ];
 
 export const Main = styled.main`
@@ -74,9 +103,9 @@ const Header = styled.header`
   background: none;
 
   @media (max-width: 430px) {
-    padding: 0 .9rem;
+    padding: 0 0.9rem;
   }
-  `;
+`;
 
 const Title = styled.h1`
   color: ${({ theme }) => theme.colors.purple};
@@ -99,24 +128,27 @@ const ContainerGrid = styled.div`
   align-items: center;
   gap: 2rem;
 
-  @media (max-width: 790px) {
+  @media (max-width: 850px) {
     grid-template-columns: 1fr 1fr;
     gap: 2rem 0;
   }
 
-  @media (max-width: 435px) {
+  @media (max-width: 570px) {
     grid-template-columns: 1fr;
   }
 `;
 
 const Card = styled.div`
-  width: 11rem;
-  height: 10rem;
-  
+  width: 15rem;
+  height: 14rem;
+
   background: #1d1d1d;
   border-radius: 10px;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
   text-align: center;
   position: relative;
 
@@ -125,121 +157,140 @@ const Card = styled.div`
   > h3 {
     color: ${({ theme }) => theme.colors.secondary};
     transition: all 0.3s;
-    font-size: .9rem;
+    font-size: 1.3rem;
     position: absolute;
-    left:0;
+    left: 0;
     right: 0;
     bottom: 20px;
     font-weight: 600;
     opacity: 1;
-    transition: .5s;
+    transition: 0.5s;
   }
 
   > span {
-    color: ${({theme}) => theme.colors.secondary};
-    font-size: 10px;
+    width: 100%;
+    color: ${({ theme }) => theme.colors.secondary};
+    font-size: 0.9rem;
     position: absolute;
-    left:0;
+    left: 0;
     bottom: 0;
     opacity: 0;
-    transition: .5s;
+    margin: 0 auto;
+    transition: 0.5s;
     padding: 5px;
-  } 
+  }
+
+  span > div {
+    position: absolute;
+    top: -2rem;
+    right: 0;
+    left: 0;
+    margin: 0 auto;
+  }
 
   > svg {
-    font-size: 2rem;
-    color: ${({theme}) => theme.colors.purple};
+    font-size: 3rem;
+    color: ${({ theme }) => theme.colors.purple};
     position: absolute;
-    top: 3rem;
-    left: 4.5rem;
+    top: 4.5rem;
+    left: 0;
     right: 0;
     bottom: 0;
-    transition: .5s;
+    margin: 0 auto;
+    transition: 0.5s;
   }
 
   > a {
     width: 4.5rem;
     padding: 2px;
-    background-color: ${({theme}) => theme.colors.purple};
+    background-color: ${({ theme }) => theme.colors.purple};
     border-radius: 10px;
-    color: ${({theme}) => theme.colors.secondary};
-    font-size: .8rem;
-    transition: all .5s;
+    color: ${({ theme }) => theme.colors.secondary};
+    font-size: 0.8rem;
+    transition: all 0.5s;
 
     position: absolute;
-    bottom: 0px;
-    left: 3.2rem;
+    bottom: 0;
+    left: 0;
     right: 0;
+    margin: 0 auto;
     opacity: 0;
     cursor: pointer;
 
     &:hover {
-      border: 1px solid ${({theme}) => theme.colors.purple};
-      background:transparent;
+      border: 1px solid ${({ theme }) => theme.colors.purple};
+      bottom: 0.5rem;
+      background: transparent;
     }
   }
 
   &:hover {
     transform: scale(1.03);
     box-shadow: rgba(0, 0, 0, 0.432) 0px 5px 15px;
-    border: 1px solid ${({theme}) => theme.colors.purple};
+    border: 1px solid ${({ theme }) => theme.colors.purple};
   }
 
-    &:hover h3 {
-      opacity: 1;
-      bottom: 6rem;
-    }
+  &:hover h3 {
+    opacity: 1;
+    bottom: 8.5rem;
+  }
 
-    &:hover span {
-      bottom: 2.5rem;
-      opacity: 1;
-    }
+  &:hover span {
+    bottom: 3rem;
+    opacity: 1;
+    margin: 0 auto;
+  }
 
-    &:hover svg {
-      font-size: 1rem;
-      top: 10px;
-      left: 10px;
-    }
+  &:hover span > div {
+    top: -3rem;
+  }
 
-    &:hover a {
-      bottom: 10px;
-      opacity: 1;
-    }
+  &:hover svg {
+    font-size: 1.5rem;
+    top: 1rem;
+  }
+
+  &:hover a {
+    bottom: 10px;
+    opacity: 1;
+  }
 `;
 
 const IconBack = styled(FaArrowLeft)`
-  color: ${({theme}) => theme.colors.purple};
+  color: ${({ theme }) => theme.colors.purple};
   font-size: 1.8rem;
   cursor: pointer;
-  transition: .5s;
+  transition: 0.5s;
 
   &:hover {
-    color: ${({theme}) => theme.colors.purpleLight};
+    color: ${({ theme }) => theme.colors.purpleLight};
   }
-`
+`;
 
 export default function Projects() {
   return (
     <>
       <Main>
-      <Header>
-        <Link href="/">
-          <IconBack/>
-        </Link>
-        <Title>Meus Projetos</Title>
-      <MenuMobile/>
-      </Header>
-      <ContainerGrid>
-        {myProjects.map((project) => (
+        <Header>
+          <Link href="/">
+            <IconBack />
+          </Link>
+          <Title>Meus Projetos</Title>
+          <MenuMobile />
+        </Header>
+        <ContainerGrid>
+          {myProjects.map((project) => (
             <Card key={project.id}>
               {project.icon}
               <h3>{project.name}</h3>
               <span>{project.description}</span>
-              <a href={project.url} target="_blank" rel="noreferrer">acessar</a>
+              <a href={project.url} target="_blank" rel="noreferrer">
+                acessar
+              </a>
             </Card>
-        ))}
-      </ContainerGrid>
-    </Main>
+          ))}
+        </ContainerGrid>
+      </Main>
     </>
   );
 }
