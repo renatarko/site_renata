d0ccd6c66005b19a7a1a0324fd7705aad04dce6b
Renata Karonlina <renatakarolinarko@gmail.com>
2023-04-16 21:22:40 -0400

    create service component

---
+++ src/components/service/index.tsx
@@ -0,0 +1,15 @@
+import * as S from "./style";
+
+export default function Service() {
+  return (
+    <S.Wrapper>
+      <S.SectionTitle>Serviços</S.SectionTitle>
+      <S.Title>Desenvolvedora</S.Title>
+      <S.SubTitle>Front-End</S.SubTitle>
+      <S.Description>
+        Principais projetos It is a long established fact that a reader will be
+        distracted by the readable content of a page when looking at its layout.
+      </S.Description>
+    </S.Wrapper>
+  );
+}
---
+++ src/components/service/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section``;
+export const SectionTitle = styled.p``;
+
+export const Title = styled.h1``;
+export const SubTitle = styled.h2``;
+export const Description = styled.p``;
