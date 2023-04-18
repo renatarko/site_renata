235919113b9c689c58233f02791746da3ac29d8d
Renata Karonlina <renatakarolinarko@gmail.com>
2023-04-17 21:01:54 -0400

    add style to article side

--- src/components/service/index.tsx
+++ src/components/service/index.tsx
@@ -3,13 +3,16 @@ import * as S from "./style";
 export default function Service() {
   return (
     <S.Wrapper>
-      <S.SectionTitle>Serviços</S.SectionTitle>
-      <S.Title>Desenvolvedora</S.Title>
-      <S.SubTitle>Front-End</S.SubTitle>
-      <S.Description>
-        Principais projetos It is a long established fact that a reader will be
-        distracted by the readable content of a page when looking at its layout.
-      </S.Description>
+      <S.Container>
+        <S.SectionTitle>Serviços</S.SectionTitle>
+        <S.Title>Desenvolvedora</S.Title>
+        <S.SubTitle>Front-End</S.SubTitle>
+        <S.Description>
+          Principais projetos It is a long established fact that a reader will
+          be distracted by the readable content of a page when looking at its
+          layout.
+        </S.Description>
+      </S.Container>
     </S.Wrapper>
   );
 }
--- src/components/service/style.ts
+++ src/components/service/style.ts
@@ -1,8 +1,43 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Wrapper = styled.section``;
-export const SectionTitle = styled.p``;
+export const Wrapper = styled.section`
+  /* width: 50%; */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
-export const Title = styled.h1``;
-export const SubTitle = styled.h2``;
-export const Description = styled.p``;
+export const SectionTitle = styled.h3`
+  ${({ theme }) => css`
+    font-size: ${theme.fontSize.titleSection};
+    color: ${theme.colors.primary};
+    text-transform: uppercase;
+    margin-bottom: 2rem;
+  `}
+`;
+
+export const Container = styled.article`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  ${({ theme }) => css`
+    font-size: ${theme.fontSize.title};
+    color: ${theme.colors.baseLight};
+    margin-bottom: -0.5rem;
+  `}
+`;
+export const SubTitle = styled.h2`
+  ${({ theme }) => css`
+    font-size: ${theme.fontSize.title};
+    color: ${theme.colors.secondary};
+  `}
+`;
+export const Description = styled.p`
+  ${({ theme }) => css`
+    font-size: ${theme.fontSize.description};
+    color: ${theme.colors.baseLight};
+    margin-top: 2.75rem;
+  `}
+`;
